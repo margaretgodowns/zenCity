c679a57dcac8d35f88dc75686db04a776ecec040
Margaret Godowns <margaretgodowns@gmail.com>
2014-10-02 16:00:11 -0400

    a few more bug fixes

--- app/scripts/filters/eventdatefilter.js
+++ app/scripts/filters/eventdatefilter.js
@@ -9,17 +9,23 @@
  * Filter in the zenCityApp.
  */
 angular.module('zenCityApp')
-  .filter('eventDateFilter', function () {
-    return function (events) {
+  .filter('eventDateFilter', function (moment) {
+    // console.log("getting into function");
+    return function (events){
+      // console.log("trying to return events");
   var eventsToday = [];
   // var eventsThisWeek = [];
   // var eventsThisMonth = [];
 
   for (var i = 0; i < events.length; i++) {
-    var currentDate = new Date();
+    // console.log("getting into for loop");
+    var currentDate = new Date().getTime();
     var dateChecked = new Date(events[i].date);
-    if (dateChecked === currentDate) {
+    if (moment(currentDate).isSame(events[i].date, 'day')) {
+      console.log("today today!")
       eventsToday.push(events[i]);
+      // console.log(dateChecked);
+      // console.log(currentDate);
     }
   }
   return eventsToday;
--- app/scripts/filters/filterpastdates.js
+++ app/scripts/filters/filterpastdates.js
@@ -9,18 +9,24 @@
  * Filter in the zenCityApp.
  */
 angular.module('zenCityApp')
-  .filter('filterPastDates', function () {
+  .filter('filterPastDates', function (moment) {
     return function (events) {
       var filterByDate = [];
       for (var i = 0; i < events.length; i++) {
-        console.log("HI");
+
         var currentDate = new Date();
-        console.log(currentDate);
-        var dateChecked = new Date(events[i].date);
-        console.log(dateChecked);
-        if (currentDate <= dateChecked) {
+
+        // var dateChecked = new Date(events[i].date).getTime();
+
+        if(moment(currentDate).isBefore(events[i].date, 'hour')) {
+          console.log("we're in!");
           filterByDate.push(events[i]);
+          console.log(filterByDate);
         }
+
+        // if (currentDate <= Date.parse(events[i].date)) {
+        //   filterByDate.push(events[i]);
+        // }
       }
       return filterByDate;
     };
